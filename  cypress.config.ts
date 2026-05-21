ec66cc6d4df6fb31c20bde6b0916dfb7073f3adc
Rusu <rusuale733@gmail.com>
2026-05-22 02:18:54 +0300

    Add cypress config in root

---
+++  cypress.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:4200',
+    specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
+    supportFile: 'cypress/support/e2e.js',
+    viewportWidth: 1280,
+    viewportHeight: 800,
+    video: false,
+    defaultCommandTimeout: 8000,
+  },
+});
